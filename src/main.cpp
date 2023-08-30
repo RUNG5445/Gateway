73821700992295e186db64f4e3884392515198e4
RUNG5445 <RUNG5445>
2023-08-30 18:39:00 +0700

    Find location using Cell id

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 #include <LoRa.h>                 // Library for LoRa communication
 #include <Wire.h>                 // I2C communication library
 
+#define SerialAT Serial1
+#define SerialMon Serial
+
+TinyGsmSim7600 modem(SerialAT);
+TinyGsmSim7600::GsmClientSim7600 client(modem);
+
 // Define Pin Configurations
 #define SerialAT Serial1
 #define SerialMon Serial
@@ -21,6 +27,7 @@
 #define ss 15
 #define rst 14
 #define dio0 13
+#define apiKey (String) "pk.71031a62fba9814c0898ae766b971df1"
 
 // Global variables
 bool DEBUG = true;
@@ -31,11 +38,35 @@ String NodeName;
 double sleepmin = 1;
 
 // LoRa configuration
-int SyncWord ;
+int SyncWord;
 int TxPower;
 long freq;
 double interval;
 int sendSyncWord = 5;
+
+// Network information
+struct NETWORK_INFO
+{
+  // Location
+  String date;
+  String time;
+  String lat;
+  String lon;
+
+  // Cell site information
+  char type[10];
+  char mode[10];
+  String mcc;
+  String mnc;
+  int lac = 0;
+  String cid;
+  char freq_b[15];
+  double rsrq = 0;
+  double rsrp = 0;
+  double rssi = 0;
+  int rssnr;
+};
+NETWORK_INFO networkinfo;
 
 void modemPowerOn()
 {
@@ -125,6 +156,85 @@ float convertCoordinate(String coordString)
   return degrees;
 }
 
+void readcellinfo()
+{
+  SerialMon.println("\n----------   Start of readcellinfo()   ----------\n");
+  // Send AT command to retrieve cell info
+  String info = sendAT("AT+CPSI?", 10000, 1);
+
+  // Process the response
+  int startIndex = info.indexOf("+CPSI: ");
+  info = info.substring(startIndex + 7, startIndex + 80);
+  info.replace("\r", "");
+  info.replace("\n", "");
+  info.replace("AT+CLBS=4ERROR", "");
+  info.replace("AT+CPSI?+CPSI: NO SERVICE", "");
+
+  // Extract the values from the response
+  int commaIndex = 0;
+  int lastCommaIndex = 0;
+  String values[15];
+  for (int i = 0; i < 15; i++)
+  {
+    commaIndex = info.indexOf(',', lastCommaIndex);
+    String temp = info.substring(lastCommaIndex, commaIndex);
+    values[i] = temp;
+    lastCommaIndex = commaIndex + 1;
+  }
+
+  // Convert and store the relevant values in the networkinfo struct
+  int lacDec = (int)strtol(values[3].c_str(), NULL, 16);
+  networkinfo.rssnr = values[13].toInt();
+  networkinfo.mcc = values[2].substring(0, 3);
+  networkinfo.mnc = values[2].substring(4, 6);
+  networkinfo.lac = lacDec;
+  networkinfo.cid = values[4];
+  delay(1000);
+  Serial.println("Base station info");
+  Serial.println("RSSNR : " + String(networkinfo.rssnr));
+  Serial.println("MCC   : " + networkinfo.mcc);
+  Serial.println("MNC   : " + networkinfo.mnc);
+  Serial.println("LAC   : " + String(networkinfo.lac));
+  Serial.println("Cell id  : " + networkinfo.cid);
+  SerialMon.println("\n----------   End of readcellinfo()   ----------\n");
+}
+
+void sendrequest()
+{
+  SerialMon.println("\n----------   Start of sendrequest()   ----------\n");
+  String payload = "{\"token\":\"" + apiKey + "\",\"radio\":\"lte\",\"mcc\":" + networkinfo.mcc + ",\"mnc\":" + networkinfo.mnc + ",\"cells\":[{\"lac\":" + networkinfo.lac + ",\"cid\":" + networkinfo.cid + ",\"psc\":0}],\"address\":1}";
+  String response;
+  client.connect("ap1.unwiredlabs.com", 80);
+  String request = "POST /v2/process.php HTTP/1.1\r\n";
+  request += "Host: ap1.unwiredlabs.com\r\n";
+  request += "Content-Type: application/x-www-form-urlencoded\r\n";
+  request += "Content-Length: ";
+  request += String(payload.length());
+  request += "\r\n\r\n";
+  request += payload;
+  client.print(request);
+  while (client.connected())
+  {
+    while (client.available())
+    {
+      char c = client.read();
+      response += c;
+      client.write(c);
+    }
+  }
+  client.stop();
+  Serial.println("Response =" + response);
+  int startIndex = response.indexOf("\"lat\":");
+  int endIndex = response.indexOf(",\"lon\":");
+  String latText = response.substring(startIndex + 6, endIndex);
+  startIndex = endIndex + 7;
+  endIndex = response.indexOf(",\"accuracy\":");
+  String lonText = response.substring(startIndex, endIndex);
+  Serial.println("Latitude: " + latText);
+  Serial.println("Longitude: " + lonText);
+  SerialMon.println("\n----------   End of sendrequest()   ----------\n");
+}
+
 void request()
 {
 
@@ -132,7 +242,8 @@ void request()
   SerialMon.println("\n----------   Start of sendrequest()   ----------\n");
 
   String http_str = "AT+HTTPPARA=\"URL\",\"https://portal.preproject.site:5000/insert_data?"
-                    "NodeName=" + NodeName +
+                    "NodeName=" +
+                    NodeName +
                     "&temp=" + temp +
                     "&humi=" + humi +
                     "&lat=" + latText +
@@ -148,7 +259,7 @@ void request()
   SerialMon.println("\n----------   End of sendrequest()   ----------\n");
 }
 
-String createJsonString(int SyncWord,int TxPower,long freq,double interval)
+String createJsonString(int SyncWord, int TxPower, long freq, double interval)
 {
   Serial.println("\n----------   Start of createJsonString()   ----------\n");
   StaticJsonDocument<512> doc;
@@ -257,7 +368,6 @@ void setup()
   modemPowerOn();
   delay(1000);
   connect2LTE();
-
 }
 
 void loop()
@@ -299,7 +409,7 @@ void loop()
     {
       LoRaData[dataIndex] = '\0';
       delay(3000);
-      String jsonOutput = createJsonString(241,20,923E6,1);
+      String jsonOutput = createJsonString(241, 20, 923E6, 1);
 
       Serial.println("Switching to sending state...");
       Serial.print("Packet send: ");
@@ -310,29 +420,30 @@ void loop()
       LoRa.endPacket();
       processJsonInput(LoRaData);
 
-      // for (int i = 0; i < 2; i++)
-      // {
-      //   // Send the AT command to enable GNSS
-      //   String response = sendAT("AT+CGNSSPWR=1", 1000, DEBUG);
+      for (int i = 0; i < 2; i++)
+      {
+        // Send the AT command to enable GNSS
+        String response = sendAT("AT+CGNSSPWR=1", 1000, DEBUG);
 
-      //   delay(2000); // Delay before the next command
+        delay(2000); // Delay before the next command
 
-      //   // Send the AT command to retrieve GNSS info
-      //   sendAT("AT+CGNSSINFO?", 1000, DEBUG);
-      //   gpsinfo = sendAT("AT+CGNSSINFO", 5000, DEBUG);
+        // Send the AT command to retrieve GNSS info
+        sendAT("AT+CGNSSINFO?", 1000, DEBUG);
+        gpsinfo = sendAT("AT+CGNSSINFO", 5000, DEBUG);
 
-      //   delay(1000); // Delay before the next iteration
-      // }
-
-      processGPSResponse(gpsinfo);
-
-      
+        if (gpsinfo = ",,,,,,")
+        {
+          readcellinfo();
+          sendrequest();
+        }
+        else
+        {
+          processGPSResponse(gpsinfo);
+        }
+      }
 
       request();
-      // esp_restart();
+      esp_restart();
     }
-
-
-
   }
 }
