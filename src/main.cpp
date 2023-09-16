83df0f7380ca0c4bf186d33c844fd2f7319b1196
RUNG5445 <RUNG5445>
2023-09-16 11:35:24 +0700

    Changing SF

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,36 @@
-// Gateway
+// Gateway LORA-Test
 
 #include <Arduino.h>
-#include <ArduinoJson.h>          // JSON library for Arduino
-#include <TinyGsmClientSIM7600.h> // Library for GSM communication using SIM7600 module
-#include <LoRa.h>                 // Library for LoRa communication
-#include <Wire.h>                 // I2C communication library
+#include <ArduinoJson.h> // JSON library for Arduino
+#include <LoRa.h>        // Library for LoRa communication
+#include <Wire.h>        // I2C communication library
 
 // Define Pin Configurations
-#define SerialAT Serial1
 #define SerialMon Serial
-#define MIN_SPACE 500000
 #define UART_BAUD 115200
-#define PIN_DTR 25
-#define PIN_TX 26
-#define PIN_RX 27
-#define PWR_PIN 4
-#define PIN_RST 5
-#define PIN_SUP 12
-#define BAT_ADC 35
 #define ss 15
 #define rst 14
 #define dio0 13
 
 // Global variables
-bool DEBUG = true;
-String response, LoRaData, latText, lonText, gpsinfo;
-float temp;
-int humi;
-String NodeName;
-double sleepmin = 1;
 int packetRssi;
 
 // LoRa configuration
-int SyncWord;
 int TxPower;
-long freq;
 double interval;
-int sendSyncWord = 5;
 
-void modemPowerOn()
-{
-  const int SUPPLY_PIN = PIN_SUP;
-  const int RESET_PIN = PIN_RST;
-  const int POWER_PIN = PWR_PIN;
-
-  pinMode(SUPPLY_PIN, OUTPUT);
-  digitalWrite(SUPPLY_PIN, HIGH);
-
-  pinMode(RESET_PIN, OUTPUT);
-  digitalWrite(RESET_PIN, LOW);
-  delay(100);
-  digitalWrite(RESET_PIN, HIGH);
-  delay(3000);
-  digitalWrite(RESET_PIN, LOW);
-
-  pinMode(POWER_PIN, OUTPUT);
-  digitalWrite(POWER_PIN, LOW);
-  delay(100);
-  digitalWrite(POWER_PIN, HIGH);
-  delay(1000);
-  digitalWrite(POWER_PIN, LOW);
-}
-
-String sendAT(String command, int interval, boolean debug)
-{
-  String response = "";
-
-  SerialAT.println(command);
-
-  long int startTime = millis();
-
-  while (((millis() - startTime)) < interval)
-  {
-    while (SerialAT.available() > 0)
-    {
-      int readData = SerialAT.read();
-      response += char(readData);
-    }
-  }
-
-  SerialAT.flush();
-
-  if (debug)
-  {
-    SerialMon.print(response);
-  }
-
-  return response;
-}
+// LoRa test configuration
+long freq = 923E6;
+int SyncWord = 0xF1;
+int spreadingFactor = 7;
+long signalBandwidth = 125E3;
+int trialtimes = 10;
 
 bool lorafind()
 {
   while (true)
   {
     int packetSize = LoRa.parsePacket();
-    // Serial.print("00000");
     if (packetSize)
     {
       Serial.print("Received packet '");
@@ -179,15 +115,27 @@ void setup()
 {
   unsigned long startTime = millis();
   SerialMon.begin(UART_BAUD);
-  SerialAT.begin(UART_BAUD, SERIAL_8N1, PIN_RX, PIN_TX);
 
   LoRa.setPins(ss, rst, dio0);
-  LoRa.setSyncWord(0xF1);
-  while (!LoRa.begin(923E6))
+  while (!LoRa.begin(freq))
   {
     Serial.println(".");
     delay(500);
   }
+
+  LoRa.setSyncWord(SyncWord);
+  LoRa.setSpreadingFactor(spreadingFactor);
+  LoRa.setSignalBandwidth(signalBandwidth);
+
+  // Initialize LoRa module
+  Serial.print("SyncWord: ");
+  Serial.println(SyncWord, HEX);
+  Serial.print("Frequency: ");
+  Serial.println(freq);
+  Serial.print("SpreadingFactor: ");
+  Serial.println(spreadingFactor);
+  Serial.print("SignalBandwidth: ");
+  Serial.println(signalBandwidth);
 
   Serial.println("LoRa Initializing OK!");
   delay(1000);
@@ -195,11 +143,11 @@ void setup()
 
 void loop()
 {
-  Serial.println("RSSI Test");
-  LoRa.setSyncWord(0xF1);
-  float RSSI = sniavg(10);
+  Serial.println("RSSI Test from " + String(trialtimes) + " values");
+
+  float RSSI = sniavg(trialtimes);
   Serial.println("-----------------------------------------------------------------------------------------------------------------");
-  Serial.print("Average of RSSI : ");
+  Serial.print("Average of RSSI from " + String(trialtimes) + " values: ");
   Serial.println(RSSI);
 
   Serial.println("\n\n\n");
